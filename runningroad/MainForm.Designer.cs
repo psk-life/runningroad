b02ee5817725e8bc26dd1b6c7f63171c4b10ec39
Dmitrii Anonymko <4jobsvac@gmail.com>
2020-02-22 20:11:56 +0300

    Добавьте файлы проекта.

---
+++ runningroad/MainForm.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace runningroad
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.trackBarDY = new System.Windows.Forms.TrackBar();
+            this.trackBarDZ = new System.Windows.Forms.TrackBar();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.trackBarOpacity = new System.Windows.Forms.TrackBar();
+            this.trackBarSize = new System.Windows.Forms.TrackBar();
+            this.ControlPanel.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.AutoSize = true;
+            this.ControlPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ControlPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ControlPanel.Controls.Add(this.label3);
+            this.ControlPanel.Controls.Add(this.label2);
+            this.ControlPanel.Controls.Add(this.label1);
+            this.ControlPanel.Controls.Add(this.groupBox2);
+            this.ControlPanel.Controls.Add(this.groupBox1);
+            this.ControlPanel.Controls.Add(this.trackBarDY);
+            this.ControlPanel.Controls.Add(this.trackBarDZ);
+            this.ControlPanel.Controls.Add(this.buttonStartStop);
+            this.ControlPanel.Controls.Add(this.trackBarOpacity);
+            this.ControlPanel.Controls.Add(this.trackBarSize);
+            this.ControlPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ControlPanel.Location = new System.Drawing.Point(0, 0);
+            this.ControlPanel.MaximumSize = new System.Drawing.Size(1920, 65);
+            this.ControlPanel.MinimumSize = new System.Drawing.Size(624, 65);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(624, 65);
+            this.ControlPanel.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(12, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Size";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(129, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Density";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(66, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Opacity";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Location = new System.Drawing.Point(321, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(126, 65);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Options";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(69, 36);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(55, 17);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Func4";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(69, 13);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(55, 17);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Func3";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 36);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "Func2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 13);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Func1";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Location = new System.Drawing.Point(255, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(65, 65);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Ghost";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Solid";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // trackBarDY
+            // 
+            this.trackBarDY.AccessibleDescription = "";
+            this.trackBarDY.LargeChange = 1;
+            this.trackBarDY.Location = new System.Drawing.Point(188, 0);
+            this.trackBarDY.Minimum = 1;
+            this.trackBarDY.MinimumSize = new System.Drawing.Size(60, 50);
+            this.trackBarDY.Name = "trackBarDY";
+            this.trackBarDY.Size = new System.Drawing.Size(60, 45);
+            this.trackBarDY.TabIndex = 4;
+            this.trackBarDY.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBarDY.Value = 3;
+            this.trackBarDY.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBarDY_MouseUp);
+            // 
+            // trackBarDZ
+            // 
+            this.trackBarDZ.AccessibleDescription = "";
+            this.trackBarDZ.LargeChange = 1;
+            this.trackBarDZ.Location = new System.Drawing.Point(122, 0);
+            this.trackBarDZ.Minimum = 1;
+            this.trackBarDZ.MinimumSize = new System.Drawing.Size(60, 50);
+            this.trackBarDZ.Name = "trackBarDZ";
+            this.trackBarDZ.Size = new System.Drawing.Size(60, 45);
+            this.trackBarDZ.TabIndex = 3;
+            this.trackBarDZ.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBarDZ.Value = 5;
+            this.trackBarDZ.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBarDZ_MouseUp);
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonStartStop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonStartStop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.buttonStartStop.Location = new System.Drawing.Point(559, 0);
+            this.buttonStartStop.MaximumSize = new System.Drawing.Size(65, 65);
+            this.buttonStartStop.MinimumSize = new System.Drawing.Size(65, 65);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(65, 65);
+            this.buttonStartStop.TabIndex = 2;
+            this.buttonStartStop.Text = "Let It Run!";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonStartStop_MouseClick);
+            // 
+            // trackBarOpacity
+            // 
+            this.trackBarOpacity.AccessibleDescription = "";
+            this.trackBarOpacity.LargeChange = 1;
+            this.trackBarOpacity.Location = new System.Drawing.Point(61, 0);
+            this.trackBarOpacity.Minimum = 1;
+            this.trackBarOpacity.MinimumSize = new System.Drawing.Size(60, 50);
+            this.trackBarOpacity.Name = "trackBarOpacity";
+            this.trackBarOpacity.Size = new System.Drawing.Size(60, 45);
+            this.trackBarOpacity.TabIndex = 1;
+            this.trackBarOpacity.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBarOpacity.Value = 10;
+            this.trackBarOpacity.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBarOpacity_MouseUp);
+            // 
+            // trackBarSize
+            // 
+            this.trackBarSize.Dock = System.Windows.Forms.DockStyle.Left;
+            this.trackBarSize.LargeChange = 1;
+            this.trackBarSize.Location = new System.Drawing.Point(0, 0);
+            this.trackBarSize.Maximum = 8;
+            this.trackBarSize.Minimum = 4;
+            this.trackBarSize.MinimumSize = new System.Drawing.Size(60, 50);
+            this.trackBarSize.Name = "trackBarSize";
+            this.trackBarSize.Size = new System.Drawing.Size(60, 65);
+            this.trackBarSize.TabIndex = 0;
+            this.trackBarSize.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBarSize.Value = 4;
+            this.trackBarSize.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBarSize_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.ControlPanel);
+            this.DoubleBuffered = true;
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Demo \"The Running Road\"";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ControlPanel.ResumeLayout(false);
+            this.ControlPanel.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ControlPanel;
+        public System.Windows.Forms.TrackBar trackBarSize;
+        public System.Windows.Forms.TrackBar trackBarOpacity;
+        private System.Windows.Forms.Button buttonStartStop;
+        public System.Windows.Forms.TrackBar trackBarDY;
+        public System.Windows.Forms.TrackBar trackBarDZ;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+    }
+}
+
---
+++ runningroad/MainForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace runningroad
+{
+    public partial class MainForm : Form
+    {
+        public int startx, starty, stopx, stopy, xx1, xx2, yy1, yy2, xx3, yy3, i, j, nn, choice1 = 1, choice2 = 4;
+        public double xcenter, ycenter, a1, n, dzy, mply, x, z, dx, dz;
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            choice2 = 5;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            choice2 = 6;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            choice2 = 3;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            choice2 = 4;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            choice1 = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            choice1 = 2;
+        }
+
+        public Pen myPen1 = new Pen(Color.White, 1);
+        public Pen myPen2 = new Pen(Color.OrangeRed, 1);
+        public Pen myPen3 = new Pen(Color.Yellow, 1);
+        public Pen myPen;
+        public Graphics g = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void MainForm_Load(object sender, EventArgs e)
+        {
+            a1 = 25.0;
+            mply = 40.0;
+            dz = 0.1;
+        }
+
+        public void trackBarOpacity_MouseUp(object sender, MouseEventArgs e)
+        {
+            MainForm.ActiveForm.Opacity = Convert.ToDouble(trackBarOpacity.Value) / 10;
+            ActiveForm.Refresh();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            ActiveForm.Refresh();
+        }
+
+        public void trackBarDZ_MouseUp(object sender, MouseEventArgs e)
+        {
+            dz = Convert.ToDouble(trackBarDZ.Value) / 50;
+        }
+
+        public void trackBarDY_MouseUp(object sender, MouseEventArgs e)
+        {
+            dzy = Convert.ToDouble(trackBarDY.Value) * 10;
+        }
+
+        public void trackBarSize_MouseUp(object sender, MouseEventArgs e)
+        {
+            MainForm.ActiveForm.Width = trackBarSize.Value * 160;
+            MainForm.ActiveForm.Height = trackBarSize.Value * 120;
+            ActiveForm.Refresh();
+        }
+
+        private void buttonStartStop_MouseClick(object sender, MouseEventArgs e)
+        {
+            RunIt();
+        }
+
+        public void RunIt()
+        {
+            ActiveForm.Refresh();
+            startx = 0;
+            stopx = MainForm.ActiveForm.Width - 17;
+            starty = 65;
+            stopy = MainForm.ActiveForm.Height - 89;
+            g = this.CreateGraphics();
+            //Rectangle r = new Rectangle(startx,starty,stopx,stopy);
+            //g.DrawRectangle(myPen1, r);
+
+            double[] hhor = new double[stopx];
+            double[] lhor = new double[stopx];
+
+            foreach (int ii in hhor)
+            {
+                hhor[ii] = Convert.ToDouble(-stopy);
+                lhor[ii] = Convert.ToDouble(stopy);
+            }
+            xcenter = (Convert.ToDouble(stopx) - Convert.ToDouble(startx)) / 2.0 + 1.0;
+            ycenter = (Convert.ToDouble(stopy) - Convert.ToDouble(starty)) * 4.0 / 3.6;
+
+            n = 4.0 * Math.PI / dz + 1.0;
+            dzy = 4.0 * Math.PI * Math.Cos(a1 * Math.PI / 180.0) / n;
+            dx = 4.0 * Math.PI / Convert.ToDouble(stopx);
+            nn = Convert.ToInt32(n);
+            for (i = 1; i <= nn; i++)
+            {
+                z = 2.0 * Math.PI - dz * (Convert.ToDouble(i) - 1.0);
+                x = -2.0 * Math.PI;
+                xx1 = xpix(x, dx);
+                yy1 = ypix(x, z, dzy, i, mply);
+                if (yy1 > hhor[0]) { hhor[0] = yy1; }
+                if (yy1 < lhor[0]) { lhor[0] = yy1; }
+
+                for (j = 1; j < stopx; j++)
+                {
+                    x = -2.0 * Math.PI + Convert.ToDouble(j) * dx;
+                    xx3 = xpix(x, dx);
+                    yy3 = ypix(x, z, dzy, i, mply);
+                    if (yy3 >= hhor[j])
+                    {
+                        hhor[j] = yy3;
+                        myPen = myPen3;
+                        mline(xx3, yy3);
+                    }
+                    if (yy3 <= lhor[j])
+                    {
+                        lhor[j] = yy3;
+                        myPen = myPen2;
+                        mline(xx3, yy3);
+                    }
+                    if ((yy3 < hhor[j]) && (yy3 > lhor[j]))
+                    {
+                        xx1 = Convert.ToInt32(xx3 + xcenter);
+                        yy1 = Convert.ToInt32(ycenter - yy3);
+                    }
+                }
+            }
+        }
+
+        public void mline(double xx, double yy)
+        {
+            xx2 = Convert.ToInt32(xx + xcenter);
+            yy2 = Convert.ToInt32(ycenter - yy);
+            drawchoice();
+            //if ((yy1 <= stopy) && (yy1 >= starty) && (yy2 <= stopy) && (yy2 >= starty) && (xx1 <= stopx) && (xx1 >= startx) && (xx2 <= stopx) && (xx2 >= startx) && (xx1 != xx2) && (yy1 != yy2))
+            //{ g.DrawLine(myPen, xx1, yy1, xx2, yy2); }
+            //g.DrawLine(myPen, xx1, yy1, xx2, yy2);
+            xx1 = xx2;
+            yy1 = yy2;
+        }
+
+        public void drawchoice()
+        {
+            switch (choice1)
+            {
+                case 1:
+                    g.DrawLine(myPen, xx1, yy1, xx2, yy2);
+                break;
+
+                case 2:
+                    if ((yy1 <= stopy) && (yy1 >= starty) && (yy2 <= stopy) && (yy2 >= starty) && (xx1 <= stopx) && (xx1 >= startx) && (xx2 <= stopx) && (xx2 >= startx) && (xx1 != xx2) && (yy1 != yy2))
+                    { g.DrawLine(myPen, xx1, yy1, xx2, yy2); }
+                break;
+            }
+        }
+
+        public double func(double x, double z, double dzy, int i)
+        {
+            double f;
+
+            switch (choice2)
+            {
+                case 3:
+                    f = 8.0 * Math.Cos(1.2 * Math.Cos(x * x + z * z)) / (Math.Sqrt(x * x + z * z) + 1.0) + dzy * Convert.ToDouble(i);
+                    return (f);
+
+                case 4:
+                    f = 8.0 * Math.Cos(1.2 * Math.Sqrt(x * x + z * z)) / (Math.Sqrt(x * x + z * z) + 1.0) + dzy * Convert.ToDouble(i);
+                    return (f);
+
+                case 5:
+                    f = 4.0 * Math.Sqrt(x * x * z * z) * (1.2 * Math.Cos(x * x + z * z)) / (Math.Sqrt(x * x + z * z) + 1.0) + dzy * Convert.ToDouble(i);
+                    return (f);
+
+                case 6:
+                    f = 2.0 * Math.Sin(x * x * z * z) * (1.2 * Math.Cos(x * x + z * z)) / (Math.Sqrt(x * x + z * z) + 1.0) + dzy * Convert.ToDouble(i);
+                    return (f);
+
+                default:
+                    f = 4.0 * Math.Sqrt(x *x*z*z)*(1.2 * Math.Cos(x * x + z * z)) / (Math.Sqrt(x * x + z * z) + 1.0) + dzy * Convert.ToDouble(i);
+                    return (f);
+            }
+        }
+
+        public int ypix(double x, double z, double dzy, int i, double mply)
+        {
+            int yp;
+            yp = Convert.ToInt32(func(x, z, dzy, i) * mply);
+            return (yp);
+        }
+
+        public int xpix(double x, double dx)
+        {
+            int xp;
+            xp = Convert.ToInt32(x / dx);
+            return (xp);
+        }
+    }
+}
